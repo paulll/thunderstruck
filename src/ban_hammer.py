5f7ea504ae13e9d6c815abe8d67c0c0f21e30bb6
paulll <paulll@derpy.ru>
2020-04-18 01:04:05 +0300

    add ability to unban

--- src/ban_hammer.py
+++ src/ban_hammer.py
@@ -31,3 +31,25 @@ async def handler(event):
 
 		banlist.add(message.from_id)
 		json.dump(list(banlist), open(banlist_path, 'w'))
+
+	if event.reply_to_msg_id and event.message.message == '!unban':
+		# От случайных людей и для прозрачности в целом
+		if event.chat_id != chat_id:
+			await client.reply("Сорри, бот принимает сообщения только из конфы")
+			return False
+
+		# Проверка на админа
+		# Просто удаляем сообщение в случае неудачи
+		admin_users = await client.get_participants(chat_id, filter=ChannelParticipantsAdmins)
+		admins = {u.id for u in admin_users}
+		if not event.from_id in admins: 
+			await client.delete_messages(event.chat_id, event.message)
+			return False
+	
+		# Удаляем сообщение админа,
+		# Убираем юзера из игнора
+		message = await client.get_messages(event.chat_id, ids=event.reply_to_msg_id)	
+		await client.delete_messages(event.chat_id, [event.message] )
+
+		banlist.remove(message.from_id)
+		json.dump(list(banlist), open(banlist_path, 'w'))	
